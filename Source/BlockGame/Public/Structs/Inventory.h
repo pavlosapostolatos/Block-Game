3438ed28867c726af7cd29140a08bcbf4daaf266
pavlos.apostolatos <pavlos.apostolatos@upstreamsystems.com>
2024-01-21 16:56:43 +0200

    attack weapon

--- Source/BlockGame/Public/Structs/Inventory.h
+++ Source/BlockGame/Public/Structs/Inventory.h
@@ -3,11 +3,20 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "BlockBox.h"
+#include "BlockGame/TP_WeaponComponent.h"
 #include "Inventory.generated.h"
+
+UENUM()
+enum class InventoryType : uint8
+{
+	BLOCK,
+	WEAPON
+};
 
 USTRUCT(BlueprintType)
 struct FInventory
 {
+
 	FInventory() = default;
 
 	GENERATED_BODY()
@@ -17,15 +26,23 @@ struct FInventory
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSubclassOf<class ABlockBox> C_Box;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	InventoryType Type;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<class UTP_WeaponComponent> C_WeaponComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	uint8 Amount;
-
 public:
-	FInventory(UTexture2D* Icon, const TSubclassOf<::ABlockBox>& C_Box, uint8 Amount)
-		: Icon(Icon),
-		  C_Box(C_Box),
-		  Amount(Amount)
+	FInventory(UTexture2D* Icon, const TSubclassOf<ABlockBox>& C_Box, InventoryType Type,
+			const TSubclassOf<UTP_WeaponComponent>& C_WeaponComponent, uint8 Amount)
+			: Icon(Icon),
+			  C_Box(C_Box),
+			  Type(Type),
+			  C_WeaponComponent(C_WeaponComponent),
+			  Amount(Amount)
 	{
 	}
 };
