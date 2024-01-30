134bc4e76ba80de22e15dc0da249aa72baa1e9d0
pavlos.apostolatos <pavlos.apostolatos@upstreamsystems.com>
2024-01-30 20:08:10 +0200

    add firing VFX

--- Source/BlockGame/BlockGame.Build.cs
+++ Source/BlockGame/BlockGame.Build.cs
@@ -6,7 +6,7 @@ public class BlockGame : ModuleRules
 {
 	public BlockGame(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PrivateDependencyModuleNames.AddRange(new string[] { "AITestSuite" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AITestSuite", "Niagara"});
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",  "SlateCore"  });
