4bb350673a484defb6827a806329c622873e5e67
pavlos.apostolatos <pavlos.apostolatos@upstreamsystems.com>
2024-01-31 10:27:48 +0200

    make a projectile interface

--- Source/BlockGame/BlockGameProjectile.h
+++ Source/BlockGame/BlockGameProjectile.h
@@ -3,35 +3,20 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Projectile.h"
 #include "GameFramework/Actor.h"
 #include "BlockGameProjectile.generated.h"
 
-class USphereComponent;
-class UProjectileMovementComponent;
-
 UCLASS(config=Game)
-class ABlockGameProjectile : public AActor
+class ABlockGameProjectile : public AProjectile
 {
 	GENERATED_BODY()
-
-	/** Sphere collision component */
-	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
-	USphereComponent* CollisionComp;
-
-	/** Projectile movement component */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	UProjectileMovementComponent* ProjectileMovement;
 
 public:
 	ABlockGameProjectile();
 
 	/** called when projectile hits something */
-	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
-
-	/** Returns CollisionComp subobject **/
-	USphereComponent* GetCollisionComp() const { return CollisionComp; }
-	/** Returns ProjectileMovement subobject **/
-	UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+					   FVector NormalImpulse, const FHitResult& Hit) override;
 };
 
